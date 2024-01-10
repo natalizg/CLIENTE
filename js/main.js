fa25ab2569c201e1004f673cef52aa2c9c86cb90
eusa-daw-ec <sebastian.mendoza@eusa.es>
2024-01-10 17:07:15 +0100

    primer commit

---
+++ js/main.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const formulario = document.getElementById("formRegistro");
+    const pasos = document.querySelectorAll(".paso");
+    const botonesSiguiente = document.querySelectorAll(".btn-siguiente");
+    const progressBar = document.querySelector(".progress-bar")
+    const botonesAnterior = document.querySelectorAll(".btn-anterior");
+    let pasoActual = 1;
+
+
+    function actualizarBarraDeProgreso() {
+        const progreso = ((pasoActual - 1) / (pasos.length - 1)) * 100;
+        console.log(progreso);
+        progressBar.style = `width: ${progreso}%`
+    }
+    function siguientePaso() {
+        pasos[pasoActual - 1].style.display = "none";
+        pasoActual++;
+        if (pasoActual > pasos.length) {
+            pasoActual = pasos.length;
+        }
+        pasos[pasoActual - 1].style.display = "block";
+        actualizarBarraDeProgreso();
+    }
+
+    function pasoAnterior() {
+        pasos[pasoActual - 1].style.display = "none";
+        pasoActual--;
+        if (pasoActual < 1) {
+            pasoActual = 1;
+        }
+        pasos[pasoActual - 1].style.display = "block";
+        actualizarBarraDeProgreso();
+    }
+
+    botonesSiguiente.forEach((boton) => {
+        boton.addEventListener("click", siguientePaso);
+    });
+
+    botonesAnterior.forEach((boton) => {
+        boton.addEventListener("click", pasoAnterior);
+    });
+
+    formulario.addEventListener("submit", (e) => e.preventDefault());
+});
