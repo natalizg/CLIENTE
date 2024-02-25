aea32eb60da3f2437f8f62996014fe653af7e5be
Natalia <natalizg1999@gmail.com>
2024-02-25 12:11:56 +0100

    paises

--- Angular Projects/PaisesApp/src/app/app.routes.ts
+++ Angular Projects/PaisesApp/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { PorPaisComponent } from './pais/pages/por-pais/por-pais.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path: '',
+        component: PorPaisComponent,
+        pathMatch: 'full'
+    },
+
+];
---
+++ Angular Projects/PaisesApp/src/app/pais/pais.module.ts
@@ -0,0 +1,29 @@
+
+/*
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { PorPaisComponent } from './pages/por-pais/por-pais.component';
+import { PaisTablaComponent } from './components/pais-tabla/pais-tabla.component';
+import { PaisInputComponent } from './components/pais-input/pais-input.component';
+
+
+
+@NgModule({
+  declarations: [
+  ],
+  exports: [
+    PorPaisComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule,
+    PorPaisComponent,
+    PaisTablaComponent,
+    PaisInputComponent
+  ]
+})
+export class PaisModule { }
+*/
